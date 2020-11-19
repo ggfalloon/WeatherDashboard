b020d47cf865846498e72aeb23c69b201b17ab23
Gabriele Falloon <ggfalloon@gmail.com>
2020-11-18 18:00:04 -0600

    unformatted 5day forecast

--- script.js
+++ script.js
@@ -8,52 +8,48 @@ $(document).ready(function () {
         var appID = "21292f97c006ec7feb138c594d793fed";
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=" + appID;
 
-
-        // var query_param = $(this).prev().val();
-
-        // if ($(this).prev().attr("placeholder") == "City Name") {
-        //     var weather = "https://api.openweathermap.org/data/2.5/weather?q=" + query_param + "&APPID=" + appID;
-        // }
-
-        // var latLon = http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + appID;
-
-        // $.getJSON(weather, function (json) {
-        //     
-        //     $("#uvindex").html(json.main.value);
-
-        //     $("#temperature").text((($("#temperature").text() * (9 / 5)) + 32));
-
-        // });
-
-        // function renderUv() {
-        // }
-
-        // var fiveDay = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + appID;
-
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
-            console.log(response.main.temp);
 
             $("#city").html(response.name);
-            // $("#weather_image").attr("src", "http://openweathermap.org/img/w/" + weather[0].icon + ".png");
+            $("#weather_image").attr("src", "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png");
             $("#temperature").html(response.main.temp);
             $("#humidity").html(response.main.humidity);
             $("#wind_speed").html(response.wind.speed);
 
+            // $("#uvindex").html(response.coord);
+            $("#cityName").val(" ");
+
+
+        });
+
+        var queryURL5 = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=" + appID;
+
+        $.ajax({
+            url: queryURL5,
+            method: "GET"
+        }).then(function (response5) {
+            console.log(response5);
+
+            var dayList = " ";
+
+            for (i = 0; i < response5.list.length; i++) {
+                dayList += "<div>";
+                dayList += "<ul>";
+                dayList += "<li>" + response5.list[i].dt_txt + "</li>";
+                dayList += "<li>" + "<img src='https://openweathermap.org/img/w/" + response5.list[i].weather[0].icon + ".png'>" + "</li>";
+                dayList += "<li>" + response5.list[i].main.temp + "</li>";
+                dayList += "<li>" + response5.list[i].main.humidity + "</li>";
+                dayList += "</ul>";
+                dayList += "</div>";
+            } $("#fiveDay").html(dayList);
 
 
         });
 
 
-    })
 
-    // function renderCityList() {
-    // }
-
-
-
-
-
+    });
 });
